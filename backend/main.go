dbe8559b6157f817e8d17106c7aebb8910b16cf1
Nakamura Shogo <nakamurashogo@NakamuranoMacBook-Air.local>
2025-10-12 22:55:46 +0900

    Initial commit: Go + Gin + Docker setup

---
+++ backend/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.String(http.StatusOK, "Hello, Gin with Docker! :cocktail:")
+    })
+
+    r.Run(":8080")
+}
