cc1945108e9987622c81f09e9f8218c8264ef4e8
Nakamura Shogo <nakamurashogo@NakamuranoMacBook-Air.local>
2025-10-17 17:54:09 +0900

    feat: 環境変数準備をし，データベース接続準備完了済み

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
 
-    r.GET("/", func(c *gin.Context) {
-        c.String(http.StatusOK, "Hello, Gin with Docker! :cocktail:")
-    })
+	r := gin.Default()
 
-    r.Run(":8080")
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, Gin with Docker! :cocktail:")
+	})
+
+	r.Run(":8080")
 }
